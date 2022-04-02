d8e6512575801c9c85fcc6c3b9c3599f1f021295
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-04-02 14:59:18 -0400

    Added a working start and stop button

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+@Output() incrementor = new EventEmitter<{incrementing:number}>();
+incrementing: number;
+start:boolean;
+  constructor() { 
+    this.incrementing=0;
+    this.start = true;
+  }
+  ngOnInit(): void {
+  }
+
+  Start()
+  {
+    setInterval(() => {
+        this.incrementing++;
+  }, 1000);
+  }
+ 
+  Stop(){
+    clearInterval();
+  }
+
+}
